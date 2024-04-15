b6165ccb15f346eb8cf248c0922dc2114c87746a
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-04-15 14:42:38 +0700

    fix imports

--- lib/parsers/detect_classes.dart
+++ lib/parsers/detect_classes.dart
@@ -1,5 +1,4 @@
 import 'package:libanime/parsers/anime.dart';
-import 'package:libanime/parsers/anime/aniboom/aniboom.dart';
 
 Map<String, dynamic> classes = {
   "kodik": Kodik(),
--- test/libanime_test.dart
+++ test/libanime_test.dart
@@ -1,4 +1,4 @@
-import '../lib/parsers/anime.dart';
+import 'package:libanime/parsers/anime.dart';
 import 'package:test/test.dart';
 
 void main() {
